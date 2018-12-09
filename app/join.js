1cd1ce4a814d52413a5e3002e4a7bc44a920388b
Timmo <contact@timmo.xyz>
2018-12-09 21:39:40 +0000

    :hammer: cleanup

--- app/join.js
+++ app/join.js
@@ -1,7 +1,5 @@
 module.exports = (wss, req, db) => {
   db.getSession(req.session.toUpperCase(), session => {
-    console.log('Session:', session);
-
     wss.clients.forEach(function each(client) {
       client.send(JSON.stringify({
         request: 'join',
